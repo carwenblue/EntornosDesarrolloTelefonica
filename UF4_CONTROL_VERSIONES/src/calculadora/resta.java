187ac9a273e3403f791b42c35de8769731f771d1
JRVaro <75578912+JRVaro@users.noreply.github.com>
2021-02-16 16:09:58 +0100

    Add files via upload

---
+++ UF4_CONTROL_VERSIONES/src/calculadora/resta.java
@@ -0,0 +1,26 @@
+package calculadora;
+
+public class resta {
+	
+	
+	static double totalAcumulado, resRestaReal;
+	static int resRestaEntera;
+	
+	
+	public static double restar(double n1,double n2){
+		return resRestaReal = n1 + n2;
+	}
+	
+	public static double Resta(int n1,int n2){
+		return n1 + n2;
+	}
+	
+	public static double restar(double n1,double n2,double n3){
+		
+		return resRestaReal = n1 + n2 + n3;
+	}
+	
+	public static double restarAcumulado(double totalAcumulado){
+		return totalAcumulado;
+	}
+}
